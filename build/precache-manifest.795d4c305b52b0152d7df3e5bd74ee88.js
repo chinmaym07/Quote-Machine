adafa7c6a13c7fba32deeee011ca7984d4afe135
chinmaym07 <b418020@iiit-bh.ac.in>
2020-09-21 16:44:36 +0530

    Adding Files for github Pages

---
+++ build/precache-manifest.795d4c305b52b0152d7df3e5bd74ee88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f71d4938783e1478ebe14e4e87d6bf91",
+    "url": "/quote-machine/index.html"
+  },
+  {
+    "revision": "f693d276c01f6048ca49",
+    "url": "/quote-machine/static/css/main.686cc663.chunk.css"
+  },
+  {
+    "revision": "6de9c892273a7cc8c3dd",
+    "url": "/quote-machine/static/js/2.9d485890.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quote-machine/static/js/2.9d485890.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f693d276c01f6048ca49",
+    "url": "/quote-machine/static/js/main.c9f6a838.chunk.js"
+  },
+  {
+    "revision": "6c1232aa5663ceebc4f7",
+    "url": "/quote-machine/static/js/runtime-main.f2084995.js"
+  }
+]);
