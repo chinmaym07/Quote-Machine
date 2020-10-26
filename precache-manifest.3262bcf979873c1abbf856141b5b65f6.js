10a99bbd6d956d50af7c368925a1c4c54f885725
chinmaym07 <b418020@iiit-bh.ac.in>
2020-10-27 03:20:09 +0530

    Updates

--- precache-manifest.3262bcf979873c1abbf856141b5b65f6.js
+++ precache-manifest.3262bcf979873c1abbf856141b5b65f6.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "32a02a1b2a01bc0c4ba464dd8d2eb56e",
+    "revision": "d8c41a1283d103436a69a0b6f9caf1f2",
     "url": "/quote-machine/index.html"
   },
   {
-    "revision": "eb35e2c6a40433e82e49",
+    "revision": "1dab1155883824ab4372",
     "url": "/quote-machine/static/css/main.b9581b86.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/quote-machine/static/js/2.9d485890.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "eb35e2c6a40433e82e49",
-    "url": "/quote-machine/static/js/main.6c0d108f.chunk.js"
+    "revision": "1dab1155883824ab4372",
+    "url": "/quote-machine/static/js/main.0df347db.chunk.js"
   },
   {
     "revision": "6c1232aa5663ceebc4f7",
