5559b9df39180f3433538147e90a1b7917f4fb93
chinmaym07 <b418020@iiit-bh.ac.in>
2020-10-27 03:20:41 +0530

    Added Build & Updated manifest.json

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quote-machine/precache-manifest.608ffed6c57bec0fff7eba823528b019.js"
+  "/quote-machine/precache-manifest.3262bcf979873c1abbf856141b5b65f6.js"
 );
 
 self.addEventListener('message', (event) => {
